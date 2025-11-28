14f4b54e33fd7c0a73f1060cad1e527d271f6b27
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-11-28 14:44:49 +0100

    Add lost-password firewall configuration to security extension

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,7 +10,6 @@ use Contao\ManagerPlugin\Config\ContainerBuilder;
 use Contao\ManagerPlugin\Config\ExtensionPluginInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use HeimrichHannot\BackendLostPasswordBundle\HeimrichHannotBackendLostPasswordBundle;
-use HeimrichHannot\UtilsBundle\StaticUtil\SUtils;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -33,13 +32,35 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface, Extension
 
     public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container): array
     {
-        // don't check for backend login
-        if ('security' === $extensionName) {
-            SUtils::array()->insertBeforeKey($extensionConfigs[0]['firewalls'], 'install', 'lost-password', [
-                'pattern' => '^/contao-be-lost-password',
-                'security' => false
-            ]);
+        if ('security' !== $extensionName) {
+            return $extensionConfigs;
         }
+
+        $extensionConfigs[0]['firewalls'] ??= [];
+        $firewalls = &$extensionConfigs[0]['firewalls'];
+
+        $newFirewall = [
+            'lost-password' => [
+                'pattern' => '^/contao-be-lost-password',
+                'security' => false,
+            ],
+        ];
+
+        $keys = \array_keys($firewalls);
+        $position = \array_search('install', $keys, true);
+
+        if ($position === false)
+        {
+            $firewalls += $newFirewall;
+
+            return $extensionConfigs;
+        }
+
+        $firewalls = \array_merge(
+            \array_slice($firewalls, 0, $position, true),
+            $newFirewall,
+            \array_slice($firewalls, $position, null, true)
+        );
 
         return $extensionConfigs;
     }
