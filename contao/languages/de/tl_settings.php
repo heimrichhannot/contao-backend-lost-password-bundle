48a2f3c0989b2e6e8362d5954833dbc934a1c6a6
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-04-09 13:46:35 +0200

    disable nc field if nc not installed

--- contao/languages/de/tl_settings.php
+++ contao/languages/de/tl_settings.php
@@ -4,3 +4,4 @@ $lang = &$GLOBALS['TL_LANG']['tl_settings'];
 
 $lang['beLostPassword_legend'] = 'Backend-Passwort-vergessen-Einstellungen';
 $lang['beLostPassword_mailerTransport'] = ['Mailer-Transport', 'Bitte wählen Sie den Mailer-Transport für das Backend-Passwort-vergessen-Feature aus.'];
+$lang['beLostPassword_nc'] = ['Benachrichtigung (Notification Center)', 'Bitte wählen Sie die Benachrichtigung aus, die beim Zurücksetzen des Passworts versendet werden soll.'];
---
+++ src/EventListener/DataContainer/Settings/ConfigOnLoadListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HeimrichHannot\BackendLostPasswordBundle\EventListener\DataContainer\Settings;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+use Terminal42\NotificationCenterBundle\NotificationCenter;
+
+class ConfigOnLoadListener
+{
+    public function __construct(
+        private readonly ?NotificationCenter $notificationCenter,
+    ) {}
+
+    #[AsCallback(table: 'tl_settings', target: 'config.onload')]
+    public function __invoke(?DataContainer $dc = null): void
+    {
+        if (null === $this->notificationCenter) {
+            $GLOBALS['TL_DCA']['tl_settings']['fields']['beLostPassword_nc']['eval']['disabled'] = true;
+        }
+    }
+}
