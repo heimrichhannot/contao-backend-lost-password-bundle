7ce8ac35183f77505a8f3743af778f7bd4d8e833
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-04-09 13:48:18 +0200

    add missing locales

--- contao/languages/en/tl_settings.php
+++ contao/languages/en/tl_settings.php
@@ -4,3 +4,4 @@ $lang = &$GLOBALS['TL_LANG']['tl_settings'];
 
 $lang['beLostPassword_legend'] = 'Backend lost password settings';
 $lang['beLostPassword_mailerTransport'] = ['Mailer transport', 'Please choose the mailer transport for the backend lost password feature.'];
+$lang['beLostPassword_nc'] = ['Notification (Notification Center)', 'Please choose the notification that should be sent when resetting the password.'];
--- contao/languages/fr/tl_settings.php
+++ contao/languages/fr/tl_settings.php
@@ -4,3 +4,4 @@ $lang = &$GLOBALS['TL_LANG']['tl_settings'];
 
 $lang['beLostPassword_legend'] = 'Paramètres d\'oubli du mot de passe du backend';
 $lang['beLostPassword_mailerTransport'] = ['Mailer transport', 'Veuillez choisir le mailer transport pour la fonctionnalité d\'oubli du mot de passe du backend.'];
+$lang['beLostPassword_nc'] = ['Notification (Notification Center)', 'Veuillez choisir la notification qui doit être envoyée lors de la réinitialisation du mot de passe.'];
